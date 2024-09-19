2f430035a9910c73150f6e8069576c373baab41c
Tiago Fialho <tiago.chem@gmail.com>
2024-09-18 23:54:57 -0300

    Category creation completed

--- create_db.sql
+++ create_db.sql
@@ -11,17 +11,17 @@ CREATE TABLE language
 CREATE INDEX IX_language_name ON language(language_name);
 
 INSERT INTO language (language_name)
-VALUES ('Portuguese'), ('English'), ('Spanish')
+VALUES ('Portuguese'), ('English'), ('Spanish'), ('French')
 GO
 
-CREATE TABLE location
+CREATE TABLE country
 (
     id SMALLINT IDENTITY(1,1),
-    country VARCHAR(50) NOT NULL,
+    country_name VARCHAR(50) NOT NULL,
     CONSTRAINT PK_country_id PRIMARY KEY(id),
-    CONSTRAINT UQ_country_name UNIQUE(country)
+    CONSTRAINT UQ_country_name UNIQUE(country_name)
 );
-CREATE INDEX IX_country_name ON location(country)
+CREATE INDEX IX_country_name ON country(country_name)
 GO
 
 CREATE TABLE author
@@ -32,7 +32,7 @@ CREATE TABLE author
     is_nobel_prize_winner BIT,
     CONSTRAINT PK_author_id PRIMARY KEY(id),
     CONSTRAINT FK_nationality_country
-        FOREIGN KEY(nationality_id) REFERENCES location(id),
+        FOREIGN KEY(nationality_id) REFERENCES country(id),
     CONSTRAINT UQ_author_name UNIQUE(name)
 );
 CREATE INDEX IX_author_name ON author(name);
@@ -45,11 +45,76 @@ CREATE TABLE publisher
     location_id SMALLINT NOT NULL,
     CONSTRAINT PK_publisher_id PRIMARY KEY(id),
     CONSTRAINT FK_publisher_country
-        FOREIGN KEY(location_id) REFERENCES location(id),
+        FOREIGN KEY(location_id) REFERENCES country(id),
     CONSTRAINT UQ_publisher_name UNIQUE(name)
 );
 CREATE INDEX IX_publisher_name ON author(name);
 GO
+
+CREATE TABLE format
+(
+    id SMALLINT IDENTITY(1,1),
+    format_name VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_format_id PRIMARY KEY(id),
+    CONSTRAINT UQ_format_name UNIQUE(format_name)
+);
+CREATE INDEX IX_format_name ON format(format_name);
+
+INSERT INTO format (format_name)
+VALUES('Paperback'), ('Hardcover'), ('Ebook'), ('Audiobook'), ('Loose Leaf'), ('Spiral-bound'),
+      ('Board Book')
+GO
+
+CREATE TABLE book_serie
+(
+    id SMALLINT IDENTITY(1,1),
+    serie_name VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_book_serie_id PRIMARY KEY(id),
+    CONSTRAINT UQ_serie_name UNIQUE(serie_name)
+);
+CREATE INDEX IX_book_serie_name ON book_serie(serie_name)
+GO
+
+CREATE TABLE award
+(
+    id SMALLINT IDENTITY(1,1),
+    award_name VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_award_id PRIMARY KEY(id),
+    CONSTRAINT UQ_award_name UNIQUE(award_name)
+);
+CREATE INDEX IX_award_name ON award(award_name);
+
+INSERT INTO award
+VALUES  ('Booker Prize'), ('Caldecott Medal'), ('Eisner Award'), ('Hugo Award'), ('Nebula Award'),
+        ('National Book Award'), ('Newbery Medal'), ('Pulitzer Price')
+GO
+
+CREATE TABLE category
+(
+    id SMALLINT IDENTITY(1,1),
+    category_name VARCHAR(50) NOT NULL,
+    CONSTRAINT PK_category_id PRIMARY KEY(id),
+    CONSTRAINT UQ_category_name UNIQUE(category_name)
+);
+CREATE INDEX IX_category_name ON category(category_name);
+
+INSERT INTO category
+VALUES
+    ('Art & Photography'), ('Biography'), ('Business & Money'), ('Childrens Book'),
+    ('Cookbook, Food & Wine'), ('Comics & Graphic Novel'), ('Computer & Technology'),
+    ('Fantasy'), ('Health, Fitness & Dieting'), ('Historical Fiction'), ('History'),
+    ('Horror'), ('Humor'), ('Law'), ('Literally Fiction'), ('Mystery'), ('Nonfiction'),
+    ('Poetry'), ('Short Story'), ('Science'), ('Science Fiction'), ('Social Science'),
+    ('Sports & Outdoors'), ('Thriller'), ('Travel')
+GO
+
+
+
+
+
+
+
+
 
 
 
--- data.sql
+++ data.sql
@@ -2,6 +2,25 @@ USE books_db;
 GO
 
 INSERT INTO language (language_name)
-VALUES ('Portuguese'), ('English'), ('Spanish')
+VALUES ('Portuguese'), ('English'), ('Spanish'), ('French');
 GO
 
+INSERT INTO format (format_name)
+VALUES('Paperback'), ('Hardcover'), ('Ebook'), ('Audiobook'), ('Loose Leaf'), ('Spiral-bound'),
+      ('Board Book');
+GO
+
+INSERT INTO award
+VALUES  ('Booker Prize'), ('Caldecott Medal'), ('Eisner Award'), ('Hugo Award'), ('Nebula Award'),
+        ('National Book Award'), ('Newbery Medal'), ('Pulitzer Price')
+GO
+
+INSERT INTO category
+VALUES
+    ('Art & Photography'), ('Biography'), ('Business & Money'), ('Childrens Book'),
+    ('Cookbook, Food & Wine'), ('Comics & Graphic Novel'), ('Computer & Technology'),
+    ('Fantasy'), ('Health, Fitness & Dieting'), ('Historical Fiction'), ('History'),
+    ('Horror'), ('Humor'), ('Law'), ('Literally Fiction'), ('Mystery'), ('Nonfiction'),
+    ('Poetry'), ('Short Story'), ('Science'), ('Science Fiction'), ('Social Science'),
+    ('Sports & Outdoors'), ('Thriller'), ('Travel')
+GO
