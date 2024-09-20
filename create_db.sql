800ab09bfd6b04d5a327fe8509618358d7875017
Tiago Fialho <tiago.chem@gmail.com>
2024-09-20 18:02:56 -0300

    correções gerais no modelo de dados

--- create_db.sql
+++ create_db.sql
@@ -24,11 +24,11 @@ CREATE TABLE author
 (
     id INT IDENTITY(1,1),
     name VARCHAR(50) NOT NULL,
-    nationality INT NOT NULL,
+    nationality_id INT NOT NULL,
     is_nobel_prize_winner BIT,
     CONSTRAINT PK_author_id PRIMARY KEY(id),
     CONSTRAINT FK_nationality_country
-        FOREIGN KEY(nationality) REFERENCES country(id),
+        FOREIGN KEY(nationality_id) REFERENCES country(id),
     CONSTRAINT UQ_author_name UNIQUE(name)
 );
 CREATE INDEX IX_author_name ON author(name);
@@ -38,10 +38,10 @@ CREATE TABLE publisher
 (
     id INT IDENTITY(1,1),
     name NVARCHAR(100) NOT NULL,
-    country SMALLINT NOT NULL,
+    country_id INT NOT NULL,
     CONSTRAINT PK_publisher_id PRIMARY KEY(id),
     CONSTRAINT FK_publisher_country
-        FOREIGN KEY(country) REFERENCES country(id),
+        FOREIGN KEY(country_id) REFERENCES country(id),
     CONSTRAINT UQ_publisher_name UNIQUE(name)
 );
 CREATE INDEX IX_publisher_name ON author(name);
@@ -137,11 +137,11 @@ CREATE TABLE store
 (
     id INT IDENTITY(1,1),
     store_name VARCHAR(50),
-    country INT NOT NULL,
+    country_id INT NOT NULL,
     CONSTRAINT PK_store_id PRIMARY KEY(id),
     CONSTRAINT UQ_store_name UNIQUE(store_name),
     CONSTRAINT FK_store_country
-        FOREIGN KEY(country) REFERENCES country(id)
+        FOREIGN KEY(country_id) REFERENCES country(id)
 );
 CREATE INDEX IX_store_name ON store(store_name);
 GO
@@ -153,45 +153,47 @@ CREATE TABLE translator
     CONSTRAINT PK_translator_id PRIMARY KEY(id),
     CONSTRAINT UQ_translator_name UNIQUE(name)
 );
+CREATE INDEX IX_translator_name ON translator(name);
+GO
+
 CREATE TABLE book
 (
     id INT IDENTITY(1,1),
     title NVARCHAR(100) NOT NULL,
-    ISBN13 CHAR(13) NOT NULL,
-    ISBN10 CHAR(10),
     pages SMALLINT NOT NULL,
-    language INT NOT NULL,
-    original_language INT NOT NULL,
-    original_country INT NOT NULL,
+    language_id INT NOT NULL,
+    original_language_id INT NOT NULL,
+    original_country_id INT NOT NULL,
     edition TINYINT NOT NULL,
+    publisher_id INT NOT NULL,
     published_at CHAR(4) NOT NULL,
     first_published_at VARCHAR(10) NOT NULL,
-    book_serie INT NOT NULL,
-    category INT NOT NULL,
-    size INT NOT NULL,
-    translator INT NOT NULL,
+    book_serie_id INT NOT NULL,
+    category_id INT NOT NULL,
+    size_id INT NOT NULL,
+    translator_id INT NOT NULL,
     CONSTRAINT PK_book_id PRIMARY KEY(id),
-    CONSTRAINT UQ_isbn UNIQUE(ISBN13),
     CONSTRAINT FK_book_language
-        FOREIGN KEY(language) REFERENCES language(id),
+        FOREIGN KEY(language_id) REFERENCES language(id),
+     CONSTRAINT FK_book_publisher
+        FOREIGN KEY(publisher_id) REFERENCES publisher(id),
     CONSTRAINT FK_book_original_language
-        FOREIGN KEY(original_language) REFERENCES language(id),
+        FOREIGN KEY(original_language_id) REFERENCES language(id),
     CONSTRAINT FK_book_original_country
-        FOREIGN KEY(original_country) REFERENCES country(id),
+        FOREIGN KEY(original_country_id) REFERENCES country(id),
     CONSTRAINT FK_book_serie
-        FOREIGN KEY(book_serie) REFERENCES book_serie(id),
+        FOREIGN KEY(book_serie_id) REFERENCES book_serie(id),
     CONSTRAINT FK_book_category
-        FOREIGN KEY(category) REFERENCES category(id),
+        FOREIGN KEY(category_id) REFERENCES category(id),
     CONSTRAINT FK_book_size
-        FOREIGN KEY(size) REFERENCES size(id),
+        FOREIGN KEY(size_id) REFERENCES size(id),
     CONSTRAINT FK_book_translator
-        FOREIGN KEY(translator) REFERENCES translator(id)
+        FOREIGN KEY(translator_id) REFERENCES translator(id)
 );
-GO
-
-CREATE INDEX IX_translator_name ON translator(name);
 CREATE INDEX IX_title ON book(title);
 GO
+
+
 
 CREATE TABLE book_author
 (
@@ -213,13 +215,13 @@ CREATE TABLE book_award
     award_id INT NOT NULL,
     CONSTRAINT PK_book_award PRIMARY KEY(book_id, award_id)
 );
-
+GO
 
 CREATE TABLE reading_info
 (
     id INT IDENTITY(1,1),
-    book INT NOT NULL,
-    reader INT NOT NULL,
+    book_id INT NOT NULL,
+    reader_id INT NOT NULL,
     started_at DATE NOT NULL DEFAULT GETDATE(),
     finished_at DATE,
     rating CHAR(1) NOT NULL,
@@ -227,33 +229,51 @@ CREATE TABLE reading_info
     is_favorite BIT DEFAULT '0',
     CONSTRAINT PK_reading_info PRIMARY KEY(id),
     CONSTRAINT FK_reading_info_book
-        FOREIGN KEY(book) REFERENCES book(id),
+        FOREIGN KEY(book_id) REFERENCES book(id),
     CONSTRAINT FK_reading_info_reader
-        FOREIGN KEY(reader) REFERENCES person(id),
+        FOREIGN KEY(reader_id) REFERENCES person(id),
     CONSTRAINT CHK_book_rating CHECK (rating >= 1 AND rating <= 5)
 );
+
+CREATE TABLE period
+(
+    id INT IDENTITY(1,1),
+    started_at DATE NOT NULL,
+    finished_at DATE NOT NULL,
+    comments VARCHAR(100),
+    CONSTRAINT PK_period PRIMARY KEY(id)
+);
+GO
+
+CREATE TABLE reading_period
+(
+    reading_info_id INT NOT NULL,
+    period_id INT NOT NULL,
+    CONSTRAINT PK_reading_period PRIMARY KEY(reading_info_id, period_id)
+);
+GO
 
 CREATE TABLE acquisition_info
 (
     id INT IDENTITY(1,1),
-    book INT NOT NULL,
-    acquirer INT NOT NULL,
-    condition INT NOT NULL,
-    acquisition_type INT NOT NULL,
+    book_id INT NOT NULL,
+    acquirer_id INT NOT NULL,
+    condition_id INT NOT NULL,
+    acquisition_type_id INT NOT NULL,
     acquired_at DATE DEFAULT GETDATE(),
     purchase_price INT NOT NULL,
-    store INT NOT NULL,
+    store_id INT NOT NULL,
     CONSTRAINT PK_acquisition_info PRIMARY KEY(id),
     CONSTRAINT FK_acquisition_info_book
-        FOREIGN KEY(book) REFERENCES book(id),
+        FOREIGN KEY(book_id) REFERENCES book(id),
     CONSTRAINT FK_acquisition_info_acquirer
-        FOREIGN KEY(acquirer) REFERENCES person(id),
+        FOREIGN KEY(acquirer_id) REFERENCES person(id),
     CONSTRAINT FK_acquisition_info_condition
-        FOREIGN KEY(condition) REFERENCES condition(id),
+        FOREIGN KEY(condition_id) REFERENCES condition(id),
     CONSTRAINT FK_acquisition_info_type
-        FOREIGN KEY(acquisition_type) REFERENCES acquisition_type(id),
+        FOREIGN KEY(acquisition_type_id) REFERENCES acquisition_type(id),
     CONSTRAINT FK_acquisition_info_store
-        FOREIGN KEY(store) REFERENCES store(id),
+        FOREIGN KEY(store_id) REFERENCES store(id),
 );
 
 
