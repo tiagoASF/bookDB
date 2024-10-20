35d2d20c4dc553e250c5105be761887d48192af6
Tiago Fialho <tiago.chem@gmail.com>
2024-10-20 19:22:39 -0300

    feat: creating usp to insert country

---
+++ stored_procedures/usp_insertCountry.sql
@@ -0,0 +1,40 @@
+USE BooksDB;
+GO
+
+IF OBJECT_ID('usp_insertCountry', 'P') IS NOT NULL
+	DROP PROCEDURE usp_insertCountry;
+GO
+
+-- =======================================================
+-- Create Stored Procedure usp_insertCountry
+-- =======================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author: Tiago Fialho
+-- Create Date: 10/20/2024
+-- Description: Insert a country in dbo.country.
+-- =============================================
+CREATE PROCEDURE usp_insertCountry (
+    @countryName nvarchar(50),
+    @country_id INT OUTPUT
+)
+AS
+BEGIN
+
+SET NOCOUNT ON
+
+INSERT INTO country
+	VALUES(@countryName)
+
+SELECT @country_id = SCOPE_IDENTITY()
+
+END
+GO
+
+/*DECLARE @newcountry_id INT;
+EXEC usp_insertCountry 'South Korea', @country_id = @newcountry_id OUTPUT
+SELECT @newcountry_id;
+GO*/
