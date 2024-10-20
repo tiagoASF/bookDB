86c3102961d6f7bd3b1eaa1199106d037b9d56d9
Tiago Fialho <tiago.chem@gmail.com>
2024-10-20 19:40:00 -0300

    feat: creating usp to insert a book serie

---
+++ stored_procedures/usp_insertBookSerie.sql
@@ -0,0 +1,40 @@
+USE BooksDB;
+GO
+
+IF OBJECT_ID('book.usp_insertBookSerie', 'P') IS NOT NULL
+	DROP PROCEDURE book.usp_insertBookSerie;
+GO
+
+-- =======================================================
+-- Create Stored Procedure usp_insertBookSerie
+-- =======================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author: Tiago Fialho
+-- Create Date: 10/20/2024
+-- Description: Insert a bookSerie in book.bookSerie
+-- =============================================
+CREATE PROCEDURE book.usp_insertBookSerie (
+    @bookSerieName nvarchar(50),
+    @bookSerie_id INT OUTPUT
+)
+AS
+BEGIN
+
+SET NOCOUNT ON
+
+INSERT INTO book.book_serie
+	VALUES(@bookSerieName)
+
+SELECT @bookSerie_id = SCOPE_IDENTITY()
+
+END
+GO
+
+/*DECLARE @newcountry_id INT;
+EXEC usp_insertCountry 'South Korea', @country_id = @newcountry_id OUTPUT
+SELECT @newcountry_id;
+GO*/
