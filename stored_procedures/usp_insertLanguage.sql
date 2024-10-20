90bcf452156ab750d5589cebdea68be2f653e3ba
Tiago Fialho <tiago.chem@gmail.com>
2024-10-20 19:30:10 -0300

    feat: creating usp to insert language

---
+++ stored_procedures/usp_insertLanguage.sql
@@ -0,0 +1,40 @@
+USE BooksDB;
+GO
+
+IF OBJECT_ID('book.usp_insertLanguage', 'P') IS NOT NULL
+	DROP PROCEDURE book.usp_insertLanguage;
+GO
+
+-- =======================================================
+-- Create Stored Procedure usp_insertLanguage
+-- =======================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author: Tiago Fialho
+-- Create Date: 10/20/2024
+-- Description: Insert a language in book.language
+-- =============================================
+CREATE PROCEDURE book.usp_insertLanguage (
+    @languageName nvarchar(50),
+    @language_id INT OUTPUT
+)
+AS
+BEGIN
+
+SET NOCOUNT ON
+
+INSERT INTO book.language
+	VALUES(@languageName)
+
+SELECT @language_id = SCOPE_IDENTITY()
+
+END
+GO
+
+/*DECLARE @newcountry_id INT;
+EXEC usp_insertCountry 'South Korea', @country_id = @newcountry_id OUTPUT
+SELECT @newcountry_id;
+GO*/
