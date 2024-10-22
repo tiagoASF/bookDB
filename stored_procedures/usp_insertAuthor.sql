d64b993a667f7b7f4f3162ea2a925a5c05d05398
Tiago Fialho <tiago.chem@gmail.com>
2024-10-22 07:26:54 -0300

    feat: creating usp to insert an author

---
+++ stored_procedures/usp_insertAuthor.sql
@@ -0,0 +1,43 @@
+IF OBJECT_ID('book.usp_insertAuthor', 'P') IS NOT NULL
+	DROP PROCEDURE book.usp_insertAuthor;
+GO
+
+-- =======================================================
+-- Create Stored Procedure usp_insertAuthor
+-- =======================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author: Tiago Fialho
+-- Create Date: 10/22/2024
+-- Description: Insert an author in book.author
+-- =============================================
+CREATE PROCEDURE book.usp_insertAuthor (
+    @name nvarchar(50),
+    @country nvarchar(50),
+    @is_nobel BIT,
+    @author_id INT OUTPUT
+)
+AS
+BEGIN
+
+SET NOCOUNT ON
+
+DECLARE @country_ID INT;
+
+IF NOT EXISTS (SELECT id FROM country WHERE country_name = @country)
+    EXEC dbo.usp_insertCountry
+        @country, @country_id = @country_ID OUTPUT;
+ELSE
+    SELECT id FROM country WHERE country_name = @country
+
+
+IF NOT EXISTS (SELECT id FROM book.author WHERE author_name = @name AND country_id = @country_ID)
+        INSERT INTO book.author (author_name, country_id, is_nobel_prize_winner)
+        VALUES(@name, @country_ID, @is_nobel)
+
+SELECT @author_id = SCOPE_IDENTITY()
+END
+GO
